55d66d908617b77d69c992be63cde4cac1f4674b
Aman kumar singh <kumarsinghaman3500@gmail.com>
2022-06-08 00:39:02 +0530

    socket.io

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io =  socketio(server);
+
+
+io.on('connection', (socket) => {
+    console.log('connected with socket id = ', socket.id);
+})
+app.use('/', express.static(__dirname + '/public'));
+
+
+
+
+server.listen('3344', () => {
+    console.log('Started on http://localhost:3344');
+})

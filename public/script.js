9b3390ceab753b4ff0760fe5fb8b89be8a11e5e3
Aman kumar singh <kumarsinghaman3500@gmail.com>
2022-06-18 23:12:36 +0530

    websockets

--- public/script.js
+++ public/script.js
@@ -1,8 +1,69 @@
+// let socket = io();
+
+// let btnSend = document.getElementById('btnSend');
+// let inpMsg = document.getElementById('inpMsg');
+// let ulMsgList = document.getElementById('ulMsgList');
+
+// if(btnSend) {
+//     btnSend.onclick = function () {
+//         socket.emit('msg_send', {
+//             msg : inpMsg.value
+//         })
+//         inpMsg.value = '';
+//     }
+// }
+
+// socket.on('msg_rcvd', (data) => {
+//     let liNewMsg = document.createElement('li');
+//     liNewMsg.innerText = data.msg;
+//     ulMsgList.appendChild(liNewMsg);
+// })
+
+// // let boomBtn = document.getElementById('boom');
+// // if(boomBtn)
+// //     boomBtn.onclick = function() {
+// //         socket.emit('boom');
+// //         console.log('aman');
+// //     }
+
+//     // socket.on('whizz', () => {
+//     //     let div = document.createElement('div');
+//     //     div.innerText = 'whizz';
+//     //     document.body.appendChild(div);
+//     // })
+
 let socket = io();
+let username;
+
+$("#loginBox").show();
+$("#chatBox").hide();
+$("#btnEnter").click(() => {
+  socket.emit("login", {
+    username: $("#inpUserName").val(),
+    password: $("#inpPass").val()
+  });
+});
+
+socket.on('login_failed', () => {
+    window.alert('Username or password is incorrect');
+})
+
+socket.on("logged_in", () => {
+  
+  $("#loginBox").hide();
+  $("#chatBox").show();
+});
 
 
-let boomBtn = document.getElementById('boom');
+$('#btnSendMsg').click(() => {
+    socket.emit('msg_send', {
+        to : $("#inpToUser").val(),
+        msg: $('#inpNewMsg').val()
+    })
+})
 
-boomBtn.onclick = function() {
-    socket.emit('boom');
-}
+socket.on('msg_rcvd', (data) => {
+    $("#ulMsgs").append($('<li>').text(
+        `[${data.from}] : ${data.msg}`
+    ))
+})
--- server.js
+++ server.js
@@ -8,11 +8,52 @@ const server = http.createServer(app);
 const io =  socketio(server);
 
 
+let users = {
+    'aman' : 'aman9211'
+}
+let socketMap = {};
+
 io.on('connection', (socket) => {
     console.log('connected with socket id = ', socket.id);
-    socket.on('boom', () => {
-        console.log('boom recieved on ', socket.id);
-    })
+    function login(s, u) {
+        s.join(u);
+        s.emit('logged_in');
+        socketMap[s.id] = u;
+        console.log(socketMap);
+    }
+//    socket.on('msg_send', (data) => {
+//     // console.log('recieved', data.msg);
+//     io.emit('msg_rcvd', data);
+//    })
+socket.on('login', (data) => {
+   if(users[data.username]) {
+        if(users[data.username] == data.password) {
+            // socket.join(data.username);
+            // socket.emit('logged_in');
+            login(socket, data.username);
+        }
+        else {
+            socket.emit('login_failed');
+        }
+   }
+   else {
+        users[data.username] = data.password;
+        login(socket, data.username);
+   }
+
+//    console.log(users);
+})
+
+
+socket.on('msg_send', (data) => {
+    data.from = socketMap[socket.id];
+    if(data.to) {
+       io.to(data.to).emit('msg_rcvd', data);
+    }
+    else {
+        socket.broadcast.emit('msg_rcvd', data);
+    }
+})
 
 })
 app.use('/', express.static(__dirname + '/public'));
